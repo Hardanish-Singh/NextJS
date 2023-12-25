7def838719e793d0bc3e47110f81ce63aeabebf8
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2023-12-25 17:23:43 -0500

    Built an API for creating issues

---
+++ issue-tracker/app/api/issues/route.ts
@@ -0,0 +1,28 @@
+import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
+import prisma from "@/prisma/client";
+
+const createIssueSchema = z.object({
+    title: z.string().min(1).max(255),
+    description: z.string().min(1),
+});
+
+export async function POST(request: NextRequest) {
+    const body = await request.json();
+    const validation = createIssueSchema.safeParse(body);
+    if (!validation.success) {
+        return NextResponse.json(validation.error.errors, { status: 400 });
+    }
+    try {
+        const newIssue = await prisma.issue.create({
+            data: {
+                title: body.title,
+                description: body.description,
+            },
+        });
+        return NextResponse.json(newIssue, { status: 201 });
+    } catch (err) {
+        console.error("Error during creation of issue", err);
+        return NextResponse.json({ data: err, success: false });
+    }
+}
