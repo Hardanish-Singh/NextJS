60fe24afd0197687e20304478fb1090df883062b
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2023-12-26 17:55:43 -0500

    Handling Form Submission Using Server Actions in NextJS

--- issue-tracker/app/issues/new/page.tsx
+++ issue-tracker/app/issues/new/page.tsx
@@ -1,15 +1,31 @@
 import { TextFieldRoot, TextFieldInput, TextArea, Button } from "@radix-ui/themes";
+import { redirect } from "next/navigation";
 import React from "react";
 
 const NewIssuePage = () => {
+    const addIssue = async (formData: FormData) => {
+        "use server";
+        const title = formData.get("title");
+        const description = formData.get("description");
+        await fetch("http://localhost:3000/api/issues", {
+            method: "POST",
+            headers: {
+                "Content-type": "application/json",
+                "API-Key": process.env.DATA_API_KEY!,
+            },
+            body: JSON.stringify({ title, description }),
+        });
+        redirect("/issues");
+    };
+
     return (
-        <div className="max-w-xl space-y-3">
+        <form className="max-w-xl space-y-3" action={addIssue}>
             <TextFieldRoot>
-                <TextFieldInput placeholder="Title"></TextFieldInput>
+                <TextFieldInput placeholder="Title" name="title"></TextFieldInput>
             </TextFieldRoot>
-            <TextArea placeholder="Description" size="3"></TextArea>
-            <Button>Submit New Issue</Button>
-        </div>
+            <TextArea placeholder="Description" size="3" name="description"></TextArea>
+            <Button type="submit">Submit New Issue</Button>
+        </form>
     );
 };
 
