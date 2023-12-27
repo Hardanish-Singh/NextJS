82111f2d2eddfe609e3411d5f85bd70f05c7a347
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2023-12-27 18:32:14 -0500

    Added a Spinner to the Submit Button

--- issue-tracker/app/issues/new/page.tsx
+++ issue-tracker/app/issues/new/page.tsx
@@ -1,16 +1,21 @@
 "use client";
 import { addIssue } from "@/actions/addIssue";
 import ErrorMessage from "@/components/ErrorMessage";
+import Spinner from "@/components/Spinner";
 import { TextFieldRoot, TextFieldInput, TextArea, Button } from "@radix-ui/themes";
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 
 const NewIssuePage = () => {
+    const [isSubmitting, SetIsSubmitting] = useState(false);
     const [error, setError] = useState({
         title: "",
         description: "",
     });
+    const ref = useRef<HTMLFormElement>(null);
     const clientAction = async (formData: FormData) => {
+        SetIsSubmitting(true);
         const result = await addIssue(formData);
+        ref.current?.reset();
         if (result?.error) {
             setError((err) => ({
                 ...err,
@@ -18,18 +23,22 @@ const NewIssuePage = () => {
                 description: result?.error?.description?._errors?.[0] ?? "",
             }));
         }
+        SetIsSubmitting(false);
     };
 
     return (
         <div className="max-w-xl">
-            <form className="space-y-3" action={clientAction}>
+            <form className="space-y-3" ref={ref} action={clientAction}>
                 <TextFieldRoot>
                     <TextFieldInput placeholder="Title" name="title"></TextFieldInput>
                 </TextFieldRoot>
                 <ErrorMessage>{error.title}</ErrorMessage>
                 <TextArea placeholder="Description" size="3" name="description"></TextArea>
                 <ErrorMessage>{error.description}</ErrorMessage>
-                <Button type="submit">Submit New Issue</Button>
+                <Button type="submit" disabled={isSubmitting}>
+                    Submit New Issue
+                    {isSubmitting && <Spinner />}
+                </Button>
             </form>
         </div>
     );
