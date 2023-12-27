8b920e8e902ad6dfd08e3f52892a8a133583fb10
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2023-12-26 20:30:06 -0500

    Handling Errors

--- issue-tracker/app/issues/new/page.tsx
+++ issue-tracker/app/issues/new/page.tsx
@@ -1,24 +1,32 @@
 "use client";
 import { addIssue } from "@/actions/addIssue";
-import { TextFieldRoot, TextFieldInput, TextArea, Button } from "@radix-ui/themes";
-import React from "react";
+import { TextFieldRoot, TextFieldInput, TextArea, Button, CalloutRoot, CalloutText } from "@radix-ui/themes";
+import React, { useState } from "react";
 
 const NewIssuePage = () => {
+    const [error, setError] = useState("");
     const clientAction = async (formData: FormData) => {
         const result = await addIssue(formData);
         if (result?.error) {
-            alert(result?.error);
+            setError(result?.error);
         }
     };
 
     return (
-        <form className="max-w-xl space-y-3" action={clientAction}>
-            <TextFieldRoot>
-                <TextFieldInput placeholder="Title" name="title"></TextFieldInput>
-            </TextFieldRoot>
-            <TextArea placeholder="Description" size="3" name="description"></TextArea>
-            <Button type="submit">Submit New Issue</Button>
-        </form>
+        <div className="max-w-xl">
+            {error && (
+                <CalloutRoot color="red" className="mb-5">
+                    <CalloutText>{error}</CalloutText>
+                </CalloutRoot>
+            )}
+            <form className="space-y-3" action={clientAction}>
+                <TextFieldRoot>
+                    <TextFieldInput placeholder="Title" name="title"></TextFieldInput>
+                </TextFieldRoot>
+                <TextArea placeholder="Description" size="3" name="description"></TextArea>
+                <Button type="submit">Submit New Issue</Button>
+            </form>
+        </div>
     );
 };
 
