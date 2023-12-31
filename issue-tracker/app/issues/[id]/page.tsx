5b0a39f05fa8a69c9f0c1039c6be09b4bd0b20ee
Hardanish Singh <hardanishsingh1@gmail.com>
2023-12-31 11:18:17 -0500

    Add the edit button

--- issue-tracker/app/issues/[id]/page.tsx
+++ issue-tracker/app/issues/[id]/page.tsx
@@ -1,7 +1,9 @@
 import IssueStatusBadge from "@/components/IssueStatusBadge";
 import prisma from "@/prisma/client";
-import { Box, Flex, Heading, ScrollArea, Text } from "@radix-ui/themes";
+import { Box, Flex, Heading, ScrollArea, Text, Grid, Button } from "@radix-ui/themes";
+import Link from "next/link";
 import { notFound } from "next/navigation";
+import { FaPencilAlt } from "react-icons/fa";
 
 type Props = {
     params: { id: number };
@@ -19,20 +21,28 @@ const page = async ({ params: { id } }: Props) => {
         notFound();
     }
     return (
-        <>
-            <Heading>{issue.title}</Heading>
-            <Flex className="space-x-3" my="2">
-                <IssueStatusBadge status={issue.status}/>
-                <Text>{issue.createdAt.toDateString()}</Text>
-            </Flex>
-            <ScrollArea scrollbars="vertical" style={{ maxHeight: 200, maxWidth: 500, border: "1px solid lightgrey", marginTop: 25 }}>
-                <Box p="2" pr="8">
-                    <Text as="p">
-                        {issue.description}
-                    </Text>
-                </Box>
-            </ScrollArea>
-        </>
+        <Grid columns={{initial: "1", md: "2"}} gap="5">
+            <Box>
+                <Heading>{issue.title}</Heading>
+                <Flex className="space-x-3" my="2">
+                    <IssueStatusBadge status={issue.status}/>
+                    <Text>{issue.createdAt.toDateString()}</Text>
+                </Flex>
+                <ScrollArea scrollbars="vertical" style={{ height: 100, maxHeight: 200, maxWidth: 500, border: "1px solid lightgrey", marginTop: 25 }}>
+                    <Box p="2" pr="8">
+                        <Text as="p">
+                            {issue.description}
+                        </Text>
+                    </Box>
+                </ScrollArea>
+            </Box>
+            <Box>
+                <Button>
+                    <FaPencilAlt />
+                    <Link href={`/issues/${id}/edit`}>Edit</Link>
+                </Button>
+            </Box>
+        </Grid>
     );
 };
 
