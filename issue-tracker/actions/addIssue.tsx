c669e2698f551c19ab166d5b63d73013947a3abb
Hardanish Singh <hardanishsingh1@gmail.com>
2024-01-07 19:07:26 -0500

    Fixed Caching Issue

--- issue-tracker/actions/addIssue.tsx
+++ issue-tracker/actions/addIssue.tsx
@@ -4,13 +4,14 @@ import { redirect } from "next/navigation";
 export const addIssue = async (formData: FormData) => {
     const title = formData.get("title");
     const description = formData.get("description");
-    const response = await fetch("http://127.0.0.1:3000/api/issues", {
+    const response = await fetch("http://localhost:3000/api/issues", {
         method: "POST",
         headers: {
             "Content-type": "application/json",
             "API-Key": process.env.DATA_API_KEY!,
         },
         body: JSON.stringify({ title, description }),
+        cache: "no-cache",
     });
     const result = await response.json();
     if (result.status != 201) {
--- issue-tracker/actions/deleteIssue.tsx
+++ issue-tracker/actions/deleteIssue.tsx
@@ -2,12 +2,13 @@
 import { redirect } from "next/navigation";
 
 export const deleteIssue = async (id: number) => {
-    const response = await fetch(`http://127.0.0.1:3000/api/issues/${id}`, {
+    const response = await fetch(`http://localhost:3000/api/issues/${id}`, {
         method: "DELETE",
         headers: {
             "Content-type": "application/json",
             "API-Key": process.env.DATA_API_KEY!,
         },
+        cache: "no-cache",
     });
     const result = await response.json();
     if (result.status === 201) {
