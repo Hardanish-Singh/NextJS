8aee5d7373c11b62440bdd21fc50b70d7f0e81b3
Hardanish Singh <hardanishsingh1@gmail.com>
2024-01-20 16:23:22 -0500

    Assignee Select Error Handling

--- issue-tracker/app/issues/_components/AssigneeSelect.tsx
+++ issue-tracker/app/issues/_components/AssigneeSelect.tsx
@@ -9,15 +9,21 @@ import {
 } from "@radix-ui/themes";
 
 const AssigneeSelect = async () => {
-    const response = await fetch("http://localhost:3000/api/users", {
-        method: "GET",
-        headers: {
-            "Content-type": "application/json",
-            "API-Key": process.env.DATA_API_KEY!,
-        },
-        cache: "no-cache",
-    });
-    const users = await response.json();
+    let users = [];
+    try {
+        const response = await fetch("http://localhost:3000/api/users", {
+            method: "GET",
+            headers: {
+                "Content-type": "application/json",
+                "API-Key": process.env.DATA_API_KEY!,
+            },
+            cache: "no-cache",
+        });
+        users = await response.json();
+    } catch (err) {
+        console.log(err);
+        return null;
+    }
     return (
         <SelectRoot>
             <SelectTrigger placeholder="Assign..." />
