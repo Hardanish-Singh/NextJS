1871b88e0a0d0c7243a817587a257e0085d1e916
Hardanish Singh <hardanishsingh1@gmail.com>
2024-02-11 09:55:48 -0500

    Removed Navbar Container

--- issue-tracker/app/Navbar.tsx
+++ issue-tracker/app/Navbar.tsx
@@ -4,7 +4,6 @@ import Spinner from "@/components/Spinner";
 import {
     Avatar,
     Box,
-    Container,
     DropdownMenuContent,
     DropdownMenuItem,
     DropdownMenuLabel,
@@ -34,75 +33,60 @@ const Navbar = () => {
     ];
     return (
         <nav className="border-b mb-5 px-5 py-3">
-            <Container>
-                <Flex justify="between">
-                    <Flex align="center" gap="3">
-                        <Link href="/">
-                            <FaBug />
-                        </Link>
-                        <ul className="flex space-x-6">
-                            {links.map(({ label, href }) => (
-                                <li key={label}>
-                                    <Link
-                                        href={href}
-                                        className={classnames({
-                                            "text-zinc-900":
-                                                currentPath === href,
-                                            "text-zinc-500":
-                                                currentPath !== href,
-                                            "hover:text-zinc-800 transition-colors":
-                                                true,
-                                        })}
-                                    >
-                                        {label}
-                                    </Link>
-                                </li>
-                            ))}
-                        </ul>
-                    </Flex>
-                    <Box>
-                        {status === "loading" && <Spinner />}
-                        {status === "authenticated" && (
-                            <>
-                                {session &&
-                                session.user &&
-                                session.user.image ? (
-                                    <DropdownMenuRoot>
-                                        <DropdownMenuTrigger>
-                                            <Avatar
-                                                src={session.user!.image!}
-                                                fallback="?"
-                                                size="2"
-                                                radius="full"
-                                                className="cursor-pointer"
-                                            />
-                                        </DropdownMenuTrigger>
-                                        <DropdownMenuContent>
-                                            <DropdownMenuLabel>
-                                                <Text size="2">
-                                                    {session.user!.email}
-                                                </Text>
-                                            </DropdownMenuLabel>
-                                            <DropdownMenuItem>
-                                                <Link href="/api/auth/signout">
-                                                    Log Out
-                                                </Link>
-                                            </DropdownMenuItem>
-                                        </DropdownMenuContent>
-                                    </DropdownMenuRoot>
-                                ) : (
-                                    <Link href="/api/auth/signout">
-                                        Log Out
-                                    </Link>
-                                )}
-                            </>
-                        )}
-                        {status === "unauthenticated" && (
-                            <Link href="/auth/signIn">Log In</Link>
-                        )}
-                    </Box>
+            <Flex justify="between">
+                <Flex align="center" gap="3">
+                    <Link href="/">
+                        <FaBug />
+                    </Link>
+                    <ul className="flex space-x-6">
+                        {links.map(({ label, href }) => (
+                            <li key={label}>
+                                <Link
+                                    href={href}
+                                    className={classnames({
+                                        "text-zinc-900": currentPath === href,
+                                        "text-zinc-500": currentPath !== href,
+                                        "hover:text-zinc-800 transition-colors": true,
+                                    })}
+                                >
+                                    {label}
+                                </Link>
+                            </li>
+                        ))}
+                    </ul>
                 </Flex>
-            </Container>
+                <Box>
+                    {status === "loading" && <Spinner />}
+                    {status === "authenticated" && (
+                        <>
+                            {session && session.user && session.user.image ? (
+                                <DropdownMenuRoot>
+                                    <DropdownMenuTrigger>
+                                        <Avatar
+                                            src={session.user!.image!}
+                                            fallback="?"
+                                            size="2"
+                                            radius="full"
+                                            className="cursor-pointer"
+                                        />
+                                    </DropdownMenuTrigger>
+                                    <DropdownMenuContent>
+                                        <DropdownMenuLabel>
+                                            <Text size="2">{session.user!.email}</Text>
+                                        </DropdownMenuLabel>
+                                        <DropdownMenuItem>
+                                            <Link href="/api/auth/signout">Log Out</Link>
+                                        </DropdownMenuItem>
+                                    </DropdownMenuContent>
+                                </DropdownMenuRoot>
+                            ) : (
+                                <Link href="/api/auth/signout">Log Out</Link>
+                            )}
+                        </>
+                    )}
+                    {status === "unauthenticated" && <Link href="/auth/signIn">Log In</Link>}
+                </Box>
+            </Flex>
         </nav>
     );
 };
