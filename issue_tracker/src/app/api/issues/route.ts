88dd4ceaaea61b20db69ccffcdd98935cc5a5d7b
Hardanish Singh <hardanishsingh1@gmail.com>
2024-09-08 17:11:30 -0400

    Issue Tracker

---
+++ issue_tracker/src/app/api/issues/route.ts
@@ -0,0 +1,37 @@
+import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
+import prisma from "../../../../prisma/client";
+
+const createIssueSchema = z.object({
+    title: z.string().min(1, "Title is required").max(255),
+    description: z.string().min(1, "Description is required"),
+});
+
+export async function POST(request: NextRequest) {
+    const body = await request.json();
+    const validation = createIssueSchema.safeParse(body);
+    if (!validation.success) {
+        return NextResponse.json({
+            data: validation.error.format(),
+            status: 400,
+        });
+    }
+    try {
+        const newIssue = await prisma.issue.create({
+            data: {
+                title: body.title,
+                description: body.description,
+            },
+        });
+        return NextResponse.json({
+            data: newIssue,
+            status: 201,
+        });
+    } catch (err) {
+        console.error("Error during creation of an issue", err);
+        return NextResponse.json({
+            data: err,
+            status: 500,
+        });
+    }
+}
