ddb968a6a94de3e14278b74395e3d590a6021bcd
Hardanish Singh <hardanishsingh1@gmail.com>
2024-09-08 18:10:33 -0400

    Issue Tracker

--- issue_tracker/src/app/layout.tsx
+++ issue_tracker/src/app/layout.tsx
@@ -1,8 +1,17 @@
 import { Theme } from "@radix-ui/themes";
 import "@radix-ui/themes/styles.css";
 import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "./Navbar";
+import "./theme-config.css";
+
+// Setup Inter Google Font and use it in the Application
+const inter = Inter({
+    subsets: ["latin"],
+    display: "swap",
+    variable: "--font-inter",
+});
 
 export const metadata: Metadata = {
     title: "Issue Tracker",
@@ -16,8 +25,8 @@ type Props = {
 const RootLayout = ({ children }: Props): React.JSX.Element => {
     return (
         <html lang="en" suppressHydrationWarning>
-            <body>
-                <Theme>
+            <body className={inter.variable}>
+                <Theme appearance="light" accentColor="violet">
                     <Navbar />
                     <main className="p-5">{children}</main>
                 </Theme>
