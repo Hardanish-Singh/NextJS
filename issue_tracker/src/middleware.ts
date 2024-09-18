1c4e72d807edb4d18fa823627e8be4b41380c514
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2024-09-18 16:40:03 -0400

    Issue Tracker

--- issue_tracker/src/middleware.ts
+++ issue_tracker/src/middleware.ts
@@ -3,9 +3,12 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function middleware(req: NextRequest, res: NextResponse) {
     const session = await auth();
+    // Below will redirect to the login page for the following routes
+    // Matches route with url pathname 'issues/new'
     if (!session && req.nextUrl.pathname === "/issues/new") {
         return NextResponse.redirect(new URL("/login", req.nextUrl));
     }
+    // Matches route with url pathname regex pattern 'issues/edit/:id'
     if (!session && /\/issues\/\d+\/edit/.test(req.nextUrl.pathname)) {
         return NextResponse.redirect(new URL("/login", req.nextUrl));
     }
