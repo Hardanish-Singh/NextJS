7447928105972eee3e9bec9d247e0d3e73b86233
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2024-09-24 07:31:55 -0400

    Issue Tracker

--- issue_tracker/src/app/logout/page.tsx
+++ issue_tracker/src/app/logout/page.tsx
@@ -3,22 +3,17 @@
 import { Button } from "@radix-ui/themes";
 import { signOut } from "next-auth/react";
 
-const SignOut = () => {
-    return (
-        <>
-            <p>Custom Sign Out Page</p>
-            <Button
-                onClick={async () => {
-                    await signOut({
-                        callbackUrl: "/",
-                        redirect: true,
-                    });
-                }}
-            >
-                Sign Out
-            </Button>
-        </>
-    );
-};
+const SignOut = () => (
+    <Button
+        onClick={async () => {
+            await signOut({
+                callbackUrl: "/",
+                redirect: true,
+            });
+        }}
+    >
+        Sign Out
+    </Button>
+);
 
 export default SignOut;
