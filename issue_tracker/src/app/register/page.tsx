f13a235c20f9215bb4fc9119f5c35dff0bf92465
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2025-01-20 08:51:20 -0500

    Issue Tracker

--- issue_tracker/src/app/register/page.tsx
+++ issue_tracker/src/app/register/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useRef } from "react";
@@ -35,10 +36,12 @@ const Register: React.FC = (): React.JSX.Element => {
         <>
             <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
                 <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-                    <img
+                    <Image
                         className="mx-auto h-10 w-auto"
-                        src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                        src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=500"
                         alt="Your Company"
+                        width="30"
+                        height="30"
                     />
                     <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
                         Register for an account
