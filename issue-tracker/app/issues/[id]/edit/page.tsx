49311d68a9dcb5e39f73a746ccd8e5a875627fbd
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2025-01-25 07:05:30 -0500

    Issue Tracker

--- issue-tracker/app/issues/[id]/edit/page.tsx
+++ issue-tracker/app/issues/[id]/edit/page.tsx
@@ -6,7 +6,7 @@ type Props = {
     params: { id: number };
 };
 
-const EditIssuePage = async ({ params: { id } }: Props) => {
+const EditIssuePage: React.FC<Props> = async ({ params: { id } }: Props) => {
     id = Number(id);
     if (isNaN(id)) {
         notFound();
