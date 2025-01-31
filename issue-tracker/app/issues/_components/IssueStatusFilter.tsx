f54e1822b49c12a2cf0a70ad86e9ca4c73743589
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2025-01-31 15:44:09 -0500

    Issue Tracker

--- issue-tracker/app/issues/_components/IssueStatusFilter.tsx
+++ issue-tracker/app/issues/_components/IssueStatusFilter.tsx
@@ -4,10 +4,10 @@ import { Status } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
 import { useRouter } from "next/navigation";
 
-const statuses: {
+const statuses: Array<{
     label: string;
     value: Status;
-}[] = [
+}> = [
     { label: "Open", value: "OPEN" },
     { label: "In Progress", value: "IN_PROGRESS" },
     { label: "Closed", value: "CLOSED" },
