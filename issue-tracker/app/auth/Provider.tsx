6cdcc8526024e5ea6e046ecbee78dbfe52161336
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2025-02-01 13:50:16 -0500

    Issue Tracker

--- issue-tracker/app/auth/Provider.tsx
+++ issue-tracker/app/auth/Provider.tsx
@@ -3,8 +3,6 @@
 import { SessionProvider } from "next-auth/react";
 import { PropsWithChildren } from "react";
 
-const AuthProvider = ({ children }: PropsWithChildren) => (
-    <SessionProvider>{children}</SessionProvider>
-);
+const AuthProvider = ({ children }: PropsWithChildren) => <SessionProvider>{children}</SessionProvider>;
 
 export default AuthProvider;
