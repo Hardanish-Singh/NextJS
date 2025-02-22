a98b29c4253e769e1dc21994ac2b7e4509dbe6d5
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2025-02-22 16:38:51 -0500

    Issue Tracker

--- issue_tracker/src/app/components/Spinner.tsx
+++ issue_tracker/src/app/components/Spinner.tsx
@@ -1,4 +1,4 @@
-const Spinner: React.FC = (): React.JSX.Element => (
+const Spinner = (): React.JSX.Element => (
     <div
         className="inline-block h-4 w-4 animate-spin rounded-full border-2 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
         role="status"
